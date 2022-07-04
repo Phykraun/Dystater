76801bb8d074e084025c758dc064e2320b22bc9a
Stanley Makazhe <devt3000@gmail.com>
2022-07-04 16:25:44 +0200

    test/bloom_tests.cpp* edits: Added test cases  bloom_create_insert_serialize/with_tweak, bloom_create_insert_key,  bloom_match, merkle_block_1/2/2_with_update_none, merkle_block_3_and_  serialize, merkle_block_4/test_p2pubkey_only.

---
+++ src/test/bloom_tests.cpp
@@ -0,0 +1,440 @@
+// Copyright (c) 2012-2013 The Bitcoin Core developers
+// Copyright (c) 2021-2022 The Dystater developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "bloom.h"
+
+#include "base58.h"
+#include "clientversion.h"
+#include "key.h"
+#include "merkleblock.h"
+#include "serialize.h"
+#include "streams.h"
+#include "uint256.h"
+#include "util.h"
+#include "utilstrencodings.h"
+
+#include <vector>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/tuple/tuple.hpp>
+
+using namespace std;
+using namespace boost::tuples;
+
+BOOST_AUTO_TEST_SUITE(bloom_tests)
+
+BOOST_AUTO_TEST_CASE(bloom_create_insert_serialize)
+{
+    CBloomFilter filter(3, 0.01, 0, BLOOM_UPDATE_ALL);
+
+    filter.insert(ParseHex("99108ad8ed9bb6274d3980bab5a85c048f0950c8"));
+    BOOST_CHECK_MESSAGE( filter.contains(ParseHex("99108ad8ed9bb6274d3980bab5a85c048f0950c8")), "BloomFilter doesn't contain just-inserted object!");
+    // One bit different in first byte
+    BOOST_CHECK_MESSAGE(!filter.contains(ParseHex("19108ad8ed9bb6274d3980bab5a85c048f0950c8")), "BloomFilter contains something it shouldn't!");
+
+    filter.insert(ParseHex("b5a2c786d9ef4658287ced5914b37a1b4aa32eee"));
+    BOOST_CHECK_MESSAGE(filter.contains(ParseHex("b5a2c786d9ef4658287ced5914b37a1b4aa32eee")), "BloomFilter doesn't contain just-inserted object (2)!");
+
+    filter.insert(ParseHex("b9300670b4c5366e95b2699e8b18bc75e5f729c5"));
+    BOOST_CHECK_MESSAGE(filter.contains(ParseHex("b9300670b4c5366e95b2699e8b18bc75e5f729c5")), "BloomFilter doesn't contain just-inserted object (3)!");
+
+    CDataStream stream(SER_NETWORK, PROTOCOL_VERSION);
+    filter.Serialize(stream, SER_NETWORK, PROTOCOL_VERSION);
+
+    vector<unsigned char> vch = ParseHex("03614e9b050000000000000001");
+    vector<char> expected(vch.size());
+
+    for (unsigned int i = 0; i < vch.size(); i++)
+        expected[i] = (char)vch[i];
+
+    BOOST_CHECK_EQUAL_COLLECTIONS(stream.begin(), stream.end(), expected.begin(), expected.end());
+
+    BOOST_CHECK_MESSAGE( filter.contains(ParseHex("99108ad8ed9bb6274d3980bab5a85c048f0950c8")), "BloomFilter doesn't contain just-inserted object!");
+    filter.clear();
+    BOOST_CHECK_MESSAGE( !filter.contains(ParseHex("99108ad8ed9bb6274d3980bab5a85c048f0950c8")), "BloomFilter should be empty!");
+}
+
+BOOST_AUTO_TEST_CASE(bloom_create_insert_serialize_with_tweak)
+{
+    // Same test as bloom_create_insert_serialize, but we add a nTweak of 100
+    CBloomFilter filter(3, 0.01, 2147483649UL, BLOOM_UPDATE_ALL);
+
+    filter.insert(ParseHex("99108ad8ed9bb6274d3980bab5a85c048f0950c8"));
+    BOOST_CHECK_MESSAGE( filter.contains(ParseHex("99108ad8ed9bb6274d3980bab5a85c048f0950c8")), "BloomFilter doesn't contain just-inserted object!");
+    // One bit different in first byte
+    BOOST_CHECK_MESSAGE(!filter.contains(ParseHex("19108ad8ed9bb6274d3980bab5a85c048f0950c8")), "BloomFilter contains something it shouldn't!");
+
+    filter.insert(ParseHex("b5a2c786d9ef4658287ced5914b37a1b4aa32eee"));
+    BOOST_CHECK_MESSAGE(filter.contains(ParseHex("b5a2c786d9ef4658287ced5914b37a1b4aa32eee")), "BloomFilter doesn't contain just-inserted object (2)!");
+
+    filter.insert(ParseHex("b9300670b4c5366e95b2699e8b18bc75e5f729c5"));
+    BOOST_CHECK_MESSAGE(filter.contains(ParseHex("b9300670b4c5366e95b2699e8b18bc75e5f729c5")), "BloomFilter doesn't contain just-inserted object (3)!");
+
+    CDataStream stream(SER_NETWORK, PROTOCOL_VERSION);
+    filter.Serialize(stream, SER_NETWORK, PROTOCOL_VERSION);
+
+    vector<unsigned char> vch = ParseHex("03ce4299050000000100008001");
+    vector<char> expected(vch.size());
+
+    for (unsigned int i = 0; i < vch.size(); i++)
+        expected[i] = (char)vch[i];
+
+    BOOST_CHECK_EQUAL_COLLECTIONS(stream.begin(), stream.end(), expected.begin(), expected.end());
+}
+
+BOOST_AUTO_TEST_CASE(bloom_create_insert_key)
+{
+    string strSecret = string("");
+    CDystaterSecret vchSecret;
+    BOOST_CHECK(vchSecret.SetString(strSecret));
+
+    CKey key = vchSecret.GetKey();
+    CPubKey pubkey = key.GetPubKey();
+    vector<unsigned char> vchPubKey(pubkey.begin(), pubkey.end());
+
+    CBloomFilter filter(2, 0.001, 0, BLOOM_UPDATE_ALL);
+    filter.insert(vchPubKey);
+    uint160 hash = pubkey.GetID();
+    filter.insert(vector<unsigned char>(hash.begin(), hash.end()));
+
+    CDataStream stream(SER_NETWORK, PROTOCOL_VERSION);
+    filter.Serialize(stream, SER_NETWORK, PROTOCOL_VERSION);
+
+    vector<unsigned char> vch = ParseHex("038fc16b080000000000000001");
+    vector<char> expected(vch.size());
+
+    for (unsigned int i = 0; i < vch.size(); i++)
+        expected[i] = (char)vch[i];
+
+    BOOST_CHECK_EQUAL_COLLECTIONS(stream.begin(), stream.end(), expected.begin(), expected.end());
+}
+
+BOOST_AUTO_TEST_CASE(bloom_match)
+{
+    // Random real transaction ()
+    CTransaction tx;
+    CDataStream stream(ParseHex(""), SER_DISK, CLIENT_VERSION);
+    stream >> tx;
+
+    // and one which spends it ()
+    unsigned char ch[] = {};
+    vector<unsigned char> vch(ch, ch + sizeof(ch) -1);
+    CDataStream spendStream(vch, SER_DISK, CLIENT_VERSION);
+    CTransaction spendingTx;
+    spendStream >> spendingTx;
+
+    CBloomFilter filter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    filter.insert(uint256(""));
+    BOOST_CHECK_MESSAGE(filter.IsRelevantAndUpdate(tx), "Simple Bloom filter didn't match tx hash");
+
+    filter = CBloomFilter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    // byte-reversed tx hash
+    filter.insert(ParseHex(""));
+    BOOST_CHECK_MESSAGE(filter.IsRelevantAndUpdate(tx), "Simple Bloom filter didn't match manually serialized tx hash");
+
+    filter = CBloomFilter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    filter.insert(ParseHex(""));
+    BOOST_CHECK_MESSAGE(filter.IsRelevantAndUpdate(tx), "Simple Bloom filter didn't match input signature");
+
+    filter = CBloomFilter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    filter.insert(ParseHex(""));
+    BOOST_CHECK_MESSAGE(filter.IsRelevantAndUpdate(tx), "Simple Bloom filter didn't match input pub key");
+
+    filter = CBloomFilter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    filter.insert(ParseHex());
+    BOOST_CHECK_MESSAGE(filter.IsRelevantAndUpdate(tx), "Simple Bloom filter didn't match output address");
+    BOOST_CHECK_MESSAGE(filter.IsRelevantAndUpdate(spendingTx), "Simple Bloom filter didn't add output");
+
+    filter = CBloomFilter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    filter.insert(ParseHex(""));
+    BOOST_CHECK_MESSAGE(filter.IsRelevantAndUpdate(tx), "Simple Bloom filter didn't match output address");
+
+    filter = CBloomFilter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    filter.insert(COutPoint(uint256(""), 0));
+    BOOST_CHECK_MESSAGE(filter.IsRelevantAndUpdate(tx), "Simple Bloom filter didn't match COutPoint");
+
+    filter = CBloomFilter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    COutPoint prevOutPoint(uint256(""), 0);
+    {
+        vector<unsigned char> data(32 + sizeof(unsigned int));
+        memcpy(&data[0], prevOutPoint.hash.begin(), 32);
+        memcpy(&data[32], &prevOutPoint.n, sizeof(unsigned int));
+        filter.insert(data);
+    }
+    BOOST_CHECK_MESSAGE(filter.IsRelevantAndUpdate(tx), "Simple Bloom filter didn't match manually serialized COutPoint");
+
+    filter = CBloomFilter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    filter.insert(uint256(""));
+    BOOST_CHECK_MESSAGE(!filter.IsRelevantAndUpdate(tx), "Simple Bloom filter matched random tx hash");
+
+    filter = CBloomFilter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    filter.insert(ParseHex(""));
+    BOOST_CHECK_MESSAGE(!filter.IsRelevantAndUpdate(tx), "Simple Bloom filter matched random address");
+
+    filter = CBloomFilter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    filter.insert(COutPoint(uint256(""), 1));
+    BOOST_CHECK_MESSAGE(!filter.IsRelevantAndUpdate(tx), "Simple Bloom filter matched COutPoint for an output we didn't care about");
+
+    filter = CBloomFilter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    filter.insert(COutPoint(uint256(""), 0));
+    BOOST_CHECK_MESSAGE(!filter.IsRelevantAndUpdate(tx), "Simple Bloom filter matched COutPoint for an output we didn't care about");
+}
+
+BOOST_AUTO_TEST_CASE(merkle_block_1)
+{
+    // Random real block ()
+    // With 9 txes
+    CBlock block;
+    CDataStream stream(ParseHex(""), SER_NETWORK, PROTOCOL_VERSION);
+    stream >> block;
+
+    CBloomFilter filter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    // Match the last transaction
+    filter.insert(uint256(""));
+
+    CMerkleBlock merkleBlock(b2lock, filter);
+    BOOST_CHECK(merkleBlock.header.GetHash() == block.GetHash());
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn.size() == 1);
+    pair<unsigned int, uint256> pair = merkleBlock.vMatchedTxn[0];
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn[0].second == uint256(""));
+    BOOST_CHECK(merkleBlock.vMatchedTxn[0].first == 8);
+
+    vector<uint256> vMatched;
+    BOOST_CHECK(merkleBlock.txn.ExtractMatches(vMatched) == block.hashMerkleRoot);
+    BOOST_CHECK(vMatched.size() == merkleBlock.vMatchedTxn.size());
+    for (unsigned int i = 0; i < vMatched.size(); i++)
+        BOOST_CHECK(vMatched[i] == merkleBlock.vMatchedTxn[i].second);
+
+    // Also match the 8th transaction
+    filter.insert(uint256(""));
+    merkleBlock = CMerkleBlock(block, filter);
+    BOOST_CHECK(merkleBlock.header.GetHash() == block.GetHash());
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn.size() == 2);
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn[1] == pair);
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn[0].second == uint256(""));
+    BOOST_CHECK(merkleBlock.vMatchedTxn[0].first == 7);
+
+    BOOST_CHECK(merkleBlock.txn.ExtractMatches(vMatched) == block.hashMerkleRoot);
+    BOOST_CHECK(vMatched.size() == merkleBlock.vMatchedTxn.size());
+    for (unsigned int i = 0; i < vMatched.size(); i++)
+        BOOST_CHECK(vMatched[i] == merkleBlock.vMatchedTxn[i].second);
+}
+
+BOOST_AUTO_TEST_CASE(merkle_block_2)
+{
+    // Random real block ()
+    // With 4 txes
+    CBlock block;
+    CDataStream stream(ParseHex(""), SER_NETWORK, PROTOCOL_VERSION);
+    stream >> block;
+
+    CBloomFilter filter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    // Match the first transaction
+    filter.insert(uint256(""));
+
+    CMerkleBlock merkleBlock(block, filter);
+    BOOST_CHECK(merkleBlock.header.GetHash() == block.GetHash());
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn.size() == 1);
+    pair<unsigned int, uint256> pair = merkleBlock.vMatchedTxn[0];
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn[0].second == uint256(""));
+    BOOST_CHECK(merkleBlock.vMatchedTxn[0].first == 0);
+
+    vector<uint256> vMatched;
+    BOOST_CHECK(merkleBlock.txn.ExtractMatches(vMatched) == block.hashMerkleRoot);
+    BOOST_CHECK(vMatched.size() == merkleBlock.vMatchedTxn.size());
+    for (unsigned int i = 0; i < vMatched.size(); i++)
+        BOOST_CHECK(vMatched[i] == merkleBlock.vMatchedTxn[i].second);
+
+    // Match an output from the second transaction (the pubkey for address -)
+    // This should match the third transaction because it spends the output matched
+    // It also matches the fourth transaction, which spends to the pubkey again
+    filter.insert(ParseHex(""));
+
+    merkleBlock = CMerkleBlock(block, filter);
+    BOOST_CHECK(merkleBlock.header.GetHash() == block.GetHash());
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn.size() == 4);
+
+    BOOST_CHECK(pair == merkleBlock.vMatchedTxn[0]);
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn[1].second == uint256(""));
+    BOOST_CHECK(merkleBlock.vMatchedTxn[1].first == 1);
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn[2].second == uint256(""));
+    BOOST_CHECK(merkleBlock.vMatchedTxn[2].first == 2);
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn[3].second == uint256(""));
+    BOOST_CHECK(merkleBlock.vMatchedTxn[3].first == 3);
+
+    BOOST_CHECK(merkleBlock.txn.ExtractMatches(vMatched) == block.hashMerkleRoot);
+    BOOST_CHECK(vMatched.size() == merkleBlock.vMatchedTxn.size());
+    for (unsigned int i = 0; i < vMatched.size(); i++)
+        BOOST_CHECK(vMatched[i] == merkleBlock.vMatchedTxn[i].second);
+}
+
+BOOST_AUTO_TEST_CASE(merkle_block_2_with_update_none)
+{
+    // Random real block ()
+    // With 4 txes
+    CBlock block;
+    CDataStream stream(ParseHex(""), SER_NETWORK, PROTOCOL_VERSION);
+    stream >> block;
+
+    CBloomFilter filter(10, 0.000001, 0, BLOOM_UPDATE_NONE);
+    // Match the first transaction
+    filter.insert(uint256(""));
+
+    CMerkleBlock merkleBlock(block, filter);
+    BOOST_CHECK(merkleBlock.header.GetHash() == block.GetHash());
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn.size() == 1);
+    pair<unsigned int, uint256> pair = merkleBlock.vMatchedTxn[0];
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn[0].second == uint256(""));
+    BOOST_CHECK(merkleBlock.vMatchedTxn[0].first == 0);
+
+    vector<uint256> vMatched;
+    BOOST_CHECK(merkleBlock.txn.ExtractMatches(vMatched) == block.hashMerkleRoot);
+    BOOST_CHECK(vMatched.size() == merkleBlock.vMatchedTxn.size());
+    for (unsigned int i = 0; i < vMatched.size(); i++)
+        BOOST_CHECK(vMatched[i] == merkleBlock.vMatchedTxn[i].second);
+
+    // Match an output from the second transaction (the pubkey for address -)
+    // This should not match the third transaction though it spends the output matched
+    // It will match the fourth transaction, which has another pay-to-pubkey output to the same address
+    filter.insert(ParseHex(""));
+
+    merkleBlock = CMerkleBlock(block, filter);
+    BOOST_CHECK(merkleBlock.header.GetHash() == block.GetHash());
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn.size() == 3);
+
+    BOOST_CHECK(pair == merkleBlock.vMatchedTxn[0]);
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn[1].second == uint256(""));
+    BOOST_CHECK(merkleBlock.vMatchedTxn[1].first == 1);
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn[2].second == uint256(""));
+    BOOST_CHECK(merkleBlock.vMatchedTxn[2].first == 3);
+
+    BOOST_CHECK(merkleBlock.txn.ExtractMatches(vMatched) == block.hashMerkleRoot);
+    BOOST_CHECK(vMatched.size() == merkleBlock.vMatchedTxn.size());
+    for (unsigned int i = 0; i < vMatched.size(); i++)
+        BOOST_CHECK(vMatched[i] == merkleBlock.vMatchedTxn[i].second);
+}
+
+BOOST_AUTO_TEST_CASE(merkle_block_3_and_serialize)
+{
+    // Random real block ()
+    // With one tx
+    CBlock block;
+    CDataStream stream(ParseHex(""), SER_NETWORK, PROTOCOL_VERSION);
+    stream >> block;
+
+    CBloomFilter filter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    // Match the only transaction
+    filter.insert(uint256(""));
+
+    CMerkleBlock merkleBlock(block, filter);
+    BOOST_CHECK(merkleBlock.header.GetHash() == block.GetHash());
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn.size() == 1);
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn[0].second == uint256(""));
+    BOOST_CHECK(merkleBlock.vMatchedTxn[0].first == 0);
+
+    vector<uint256> vMatched;
+    BOOST_CHECK(merkleBlock.txn.ExtractMatches(vMatched) == block.hashMerkleRoot);
+    BOOST_CHECK(vMatched.size() == merkleBlock.vMatchedTxn.size());
+    for (unsigned int i = 0; i < vMatched.size(); i++)
+        BOOST_CHECK(vMatched[i] == merkleBlock.vMatchedTxn[i].second);
+
+    CDataStream merkleStream(SER_NETWORK, PROTOCOL_VERSION);
+    merkleStream << merkleBlock;
+
+    vector<unsigned char> vch = ParseHex("");
+    vector<char> expected(vch.size());
+
+    for (unsigned int i = 0; i < vch.size(); i++)
+        expected[i] = (char)vch[i];
+
+    BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), merkleStream.begin(), merkleStream.end());
+}
+
+BOOST_AUTO_TEST_CASE(merkle_block_4)
+{
+    // Random real block ()
+    // With 7 txes
+    CBlock block;
+    CDataStream stream(ParseHex(""), SER_NETWORK, PROTOCOL_VERSION);
+    stream >> block;
+
+    CBloomFilter filter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
+    // Match the last transaction
+    filter.insert(uint256(""));
+
+    CMerkleBlock merkleBlock(block, filter);
+    BOOST_CHECK(merkleBlock.header.GetHash() == block.GetHash());
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn.size() == 1);
+    pair<unsigned int, uint256> pair = merkleBlock.vMatchedTxn[0];
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn[0].second == uint256(""));
+    BOOST_CHECK(merkleBlock.vMatchedTxn[0].first == 6);
+
+    vector<uint256> vMatched;
+    BOOST_CHECK(merkleBlock.txn.ExtractMatches(vMatched) == block.hashMerkleRoot);
+    BOOST_CHECK(vMatched.size() == merkleBlock.vMatchedTxn.size());
+    for (unsigned int i = 0; i < vMatched.size(); i++)
+        BOOST_CHECK(vMatched[i] == merkleBlock.vMatchedTxn[i].second);
+
+    // Also match the 4th transaction
+    filter.insert(uint256(""));
+    merkleBlock = CMerkleBlock(block, filter);
+    BOOST_CHECK(merkleBlock.header.GetHash() == block.GetHash());
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn.size() == 2);
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn[0].second == uint256(""));
+    BOOST_CHECK(merkleBlock.vMatchedTxn[0].first == 3);
+
+    BOOST_CHECK(merkleBlock.vMatchedTxn[1] == pair);
+
+    BOOST_CHECK(merkleBlock.txn.ExtractMatches(vMatched) == block.hashMerkleRoot);
+    BOOST_CHECK(vMatched.size() == merkleBlock.vMatchedTxn.size());
+    for (unsigned int i = 0; i < vMatched.size(); i++)
+        BOOST_CHECK(vMatched[i] == merkleBlock.vMatchedTxn[i].second);
+}
+
+BOOST_AUTO_TEST_CASE(merkle_block_4_test_p2pubkey_only)
+{
+    // Random real block ()
+    // With 7 txes
+    CBlock block;
+    CDataStream stream(ParseHex(""), SER_NETWORK, PROTOCOL_VERSION);
+    stream >> block;
+
+    CBloomFilter filter(10, 0.000001, 0, BLOOM_UPDATE_P2PUBKEY_ONLY);
+    // Match the generation pubkey
+    filter.insert(ParseHex(""));
+    // ...and the output address of the 4th transaction
+    filter.insert(ParseHex(""));
+
+    CMerkleBlock merkleBlock(block, filter);
+    BOOST_CHECK(merkleBlock.header.GetHash() == block.GetHash());
+
+    // We should match the generation outpoint
+    BOOST_CHECK(filter.contains(COutPoint(uint256(""))));
+    // ... but not the 4th transaction's
+}
+
+BOOST_AUTO_TEST_SUITE_END()
