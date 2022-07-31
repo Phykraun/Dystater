283b2e717834cc5b21866f8d310b2ff1d9d8b4b1
Stanley Makazhe <devt3000@gmail.com>
2022-07-31 04:57:28 +0200

    qt/optionsdialog.h/optionsmodel.h/overviewpage.h  /notificator.h edits: Dyst ver 2.0/30.

---
+++ src/qt/optionsdialog.h
@@ -0,0 +1,58 @@
+// Copyright (c) 2011-2013 The Bitcoin developers
+// Copyright (c) 2022 The Dystater developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef OPTIONSDIALOG_H
+#define OPTIONSDIALOG_H
+
+#include <QDialog>
+
+QT_BEGIN_NAMESPACE
+class QStackedWidget;
+class QListWidget;
+class QListWidgetItem;
+class QPushButton;
+QT_END_NAMESPACE
+class OptionsModel;
+class MainOptionsPage;
+class DisplayOptionsPage;
+class MonitoredDataMapper;
+
+/** Preferences dialog. */
+class OptionsDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit OptionsDialog(QWidget *parent=0);
+
+    void setModel(OptionsModel *model);
+
+signals:
+
+public slots:
+    /** Change the current page to \a index. */
+    void changePage(int index);
+
+private slots:
+    void okClicked();
+    void cancelClicked();
+    void applyClicked();
+    void enableApply();
+    void disableApply();
+
+private:
+    QListWidget *contents_widget;
+    QStackedWidget *pages_widget;
+    OptionsModel *model;
+    MonitoredDataMapper *mapper;
+    QPushButton *apply_button;
+
+    // Pages
+    MainOptionsPage *main_page;
+    DisplayOptionsPage *display_page;
+
+    void setupMainPage();
+};
+
+#endif // OPTIONSDIALOG_H
---
+++ src/qt/overviewpage.h
@@ -0,0 +1,51 @@
+// Copyright (c) 2011-2013 The Bitcoin developers
+// Copyright (c) 2022 The Dystater developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef OVERVIEWPAGE_H
+#define OVERVIEWPAGE_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+class QModelIndex;
+QT_END_NAMESPACE
+
+namespace Ui {
+    class OverviewPage;
+}
+class WalletModel;
+class TxViewDelegate;
+
+/** Overview ("home") page widget */
+class OverviewPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit OverviewPage(QWidget *parent = 0);
+    ~OverviewPage();
+
+    void setModel(WalletModel *model);
+
+public slots:
+    void setBalance(qint64 balance, qint64 unconfirmedBalance);
+    void setNumTransactions(int count);
+
+signals:
+    void transactionClicked(const QModelIndex &index);
+
+private:
+    Ui::OverviewPage *ui;
+    WalletModel *model;
+    qint64 currentBalance;
+    qint64 currentUnconfirmedBalance;
+
+    TxViewDelegate *txdelegate;
+
+private slots:
+    void displayUnitChanged();
+};
+
+#endif // OVERVIEWPAGE_H
